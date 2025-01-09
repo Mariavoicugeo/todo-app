3e0ca237c6d83d246a3177b09c7a98c3cb5e4748
maria <164060667+Mariavoicugeo@users.noreply.github.com>
2025-01-09 14:11:24 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-app/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/todo-app/', // Replace with your repository name
 });
